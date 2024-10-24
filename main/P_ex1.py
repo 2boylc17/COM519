b5fb7275e6c5b0561e6de070edc917723e212276
Callum Boylan <2boylc17@solent.ac.uk>
2024-10-24 14:48:21 +0100

    Week 5

--- main/P_ex1.py
+++ main/P_ex1.py
@@ -9,14 +9,16 @@ tkWindow = Tk()
 tkWindow.geometry('400x250')  
 tkWindow.title('Tkinter SQLite Login Form example ')
 
-def validateLogin(username, password):
+def validate_login(user_name, pass_word):
         connection_obj = sqlite3.connect("newdb.db")
         cursor_obj = connection_obj.cursor()
 
-        value_username = username.get()
-        value_password = str(password.get())
+        value_username = user_name.get()
+        value_password = str(pass_word.get())
+        dec_username = hashlib.sha256(value_username.encode()).hexdigest()
+        dec_password = hashlib.sha256(value_password.encode()).hexdigest()
         cursor_obj.execute(
-                "SELECT * FROM login_details WHERE User_Login='" + value_username + "'" + " AND User_password='" + value_password + "'")
+                "SELECT * FROM login_details WHERE User_Login='" + dec_username + "'" + " AND User_password='" + dec_password + "'")
         output1 = cursor_obj.fetchall()
 
         if len(output1) > 0:
@@ -35,20 +37,22 @@ def validateLogin(username, password):
         connection_obj.close()
        
 
-def register_customer(username, password):
+def register_customer(user_name, pass_word):
 	
         connection_obj = sqlite3.connect("newdb.db")
         cursor_obj = connection_obj.cursor()
-        value_username=username.get()
-        value_password=str(password.get())
+        value_username=user_name.get()
+        value_password=str(pass_word.get())
         #encrypton
         #encode(value_password, password = 'mypass')
+        enc_username = hashlib.sha256(value_username.encode()).hexdigest()
+        enc_password = hashlib.sha256(value_password.encode()).hexdigest()
         
         value_password.encode()
         
         if len(value_password)>5 and len(value_username)>5:
                 #cursor_obj.execute("insert into login_details values('"+value_username+"', '"+str(encvalue_password)+"')")
-                cursor_obj.execute("insert into login_details values('"+value_username+"', '"+value_password+"')")
+                cursor_obj.execute("insert into login_details values('"+enc_username+"', '"+enc_password+"')")
                 messagebox.showinfo("showinfo", "user registred")
         else:
                 messagebox.showwarning("Warning","Incorrect length of password and login - min 5 symbols")
@@ -69,7 +73,7 @@ passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)
 password = StringVar()
 passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)  
 
-validateLogin = partial(validateLogin, username, password)
+validateLogin = partial(validate_login, username, password)
 
 # Login button
 loginButton1 = Button(tkWindow, text="Login", command=validateLogin).grid(row=3, column=0)
